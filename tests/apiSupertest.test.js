3e31a29260f5613af6e4080af55d5dbcd68764a9
Pedro <pbelosoberdun@gmail.com>
2025-11-23 18:07:09 -0300

    Super Test

---
+++ tests/apiSupertest.test.js
@@ -0,0 +1,47 @@
+import { expect } from "chai";
+import supertest from "supertest";
+import app from "../server.js";
+import mongoose from "mongoose";
+
+
+/* 1) instalar en la consola: npm install supertest --save-dev */
+/* 2) ejecutar en consola: npm run testSuper */
+
+const request = supertest(app);
+
+describe("SUPERTEST - Pruebas de API Adopciones", function () {
+
+    before(async () => {
+        /*  Conectar a la base de datos antes de las pruebas */
+        if (mongoose.connection.readyState === 0) {
+            await mongoose.connect(process.env.MONGO_URI);
+        }
+    });
+
+    after(async () => {
+        /* cierra la conexion */
+        await mongoose.connection.close();
+    });
+
+    it("GET /api/animales → debería devolver una lista y status 200", async () => {
+        const response = await request.get("/api/animales");
+        console.log("GET animales:", response.status, response.body);
+        expect(response.status).to.equal(200);
+    });
+
+    it("POST /api/usuarios → debería crear un usuario", async () => {
+        const usuario = {
+            nombre: "Test",
+            apellido: "Supertest",
+            correo: "test@example.com",
+            password: "12345678",
+            fechaNacimiento: "2000-01-01"
+        };
+
+        const response = await request.post("/api/usuarios").send(usuario);
+        console.log("POST usuarios:", response.status, response.body);
+
+        expect(response.status).to.equal(201);
+    });
+
+});
